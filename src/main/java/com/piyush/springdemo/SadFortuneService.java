4d0b90e5567718de532df046f0fabc04c0ef8aa3
Piyush Tiwari <piyushtiwari1@gmail.com>
2018-09-03 18:15:38 +0530

    .

---
+++ src/main/java/com/piyush/springdemo/SadFortuneService.java
@@ -0,0 +1,7 @@
+package com.piyush.springdemo;
+
+public class SadFortuneService implements FortuneService {
+    public String getFortune() {
+        return "Today is a sad day";
+    }
+}
---
+++ src/main/java/com/piyush/springdemo/SwimCoach.java
@@ -0,0 +1,34 @@
+package com.piyush.springdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwimCoach implements Coach {
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
+    private FortuneService fortuneService;
+
+    public SwimCoach(FortuneService fortuneService){
+        this.fortuneService = fortuneService;
+    }
+
+    public String getDailyWorkout() {
+        return "Swim 1000 m as a warm up";
+    }
+
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+}
